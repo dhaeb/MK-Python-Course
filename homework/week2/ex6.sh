4a6c2f8cf23d6aacb4ff937b8c5e06f1217f7920
Dan Häberlein <dan.haeberlein@googlemail.com>
2014-04-22 17:14:15 +0200

    put all exercise artifacts into the homework folder

---
+++ homework/week2/ex6.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python3 ex6_create_dict.py data/ex6_ex1_key_values.txt
+python3 ex6_parse_csv_to_list.py data/test.csv
+python3 ex6_parse_csv_to_dict.py data/ex6_ex3_sample.csv 
+
